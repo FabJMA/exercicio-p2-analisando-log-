351039fa1f81115547e49a013d114fc2a35dd746
Fabi <fabistudy016@gmail.com>
2026-05-01 19:36:27 -0300

    Passando os códigos que tinham sidos feitos

---
+++ src/main/java/log/analisador/log/analisador/log/RegexLog.java
@@ -0,0 +1,20 @@
+package log.analisador.log.analisador.log;
+
+public class RegexLog {
+	
+	public void verificaGrandeRespondido() {
+		
+	};
+	
+	public void verificaNaoRespondidos() {
+		
+	};
+	
+	public void requisicoesSo() {
+		
+	};
+	
+	public void mediaRequisicoes() {
+		
+	};
+}
