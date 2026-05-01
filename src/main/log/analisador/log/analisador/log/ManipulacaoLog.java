f19ccf1bbcf203af71382cf05347351984464357
EricFelix <ericfelixfelixpinto@gmail.com>
2026-05-01 20:41:37 -0300

    Adicionando a funcionalidade do regex para o log da opcao 1 do menu

--- src/main/log/analisador/log/analisador/log/ManipulacaoLog.java
+++ src/main/log/analisador/log/analisador/log/ManipulacaoLog.java
@@ -1,3 +1,49 @@
 public class ManipulacaoLog {
+    private String ip;
+    private String data;
+    private String recurso;
+    private int codigo;
+    private long tamanhoObjeto;
+    private String sistemaOperacional;
+    private String userAgent;
+
+    public ManipulacaoLog(String ip, String data, String recurso, int codigo, int tamanhoObjeto, String sistemaOperacional, String userAgent){
+        this.ip = ip;
+        this.data = data;
+        this.recurso = recurso;
+        this.codigo = codigo;
+        this.tamanhoObjeto = tamanhoObjeto;
+        this.sistemaOperacional = sistemaOperacional;
+        this.userAgent = userAgent;
+    }
+    
+    public String getIp() {
+        return ip;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getRecurso() {
+        return recurso;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public long getTamanhoObjeto() {
+        return tamanhoObjeto;
+    }
+
+    public String getSistemaOperacional() {
+        return sistemaOperacional;
+    }
+
+    public String getUserAgent() {
+        return userAgent;
+    }
+
 
 }
--- src/main/log/analisador/log/analisador/log/RegexLog.java
+++ src/main/log/analisador/log/analisador/log/RegexLog.java
@@ -1,3 +1,22 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexLog {
+    
+    public String buscaRecursoGrande(String linha) {
+        Pattern pattern = Pattern.compile("([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}) \\[.*?\\] \"[^\"]*\" ([0-9]{3}) ([0-9]+)");
+        Matcher matcher = pattern.matcher(linha);
+
+        if (matcher.find()) {
+            String ip = matcher.group(1);
+            int status = Integer.parseInt(matcher.group(2));
+            long tamanho = Long.parseLong(matcher.group(3));
+
+            if (status >= 200 && status <= 299 && tamanho > 2000) {
+                return status + " " + tamanho + " " + ip;
+            }
+        }
+        return null;
+    }
 
 }
