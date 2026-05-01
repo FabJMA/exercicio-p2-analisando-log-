fa2853fb145c7de313b921cd4332a713d40eba08
EricFelix <ericfelixfelixpinto@gmail.com>
2026-04-30 23:29:31 -0300

    Estruturando o início do projeto, com algumas classes essenciais

--- src/main/log/analisador/Main.java
+++ src/main/log/analisador/Main.java
@@ -1,9 +1,6 @@
 package log.analisador;
 
-/**
- * Hello world!
- */
-public class App {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
