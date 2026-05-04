8a38cdbf189b0bf695bd4a0c32a8fe75404a0da7
Fabi <fabistudy016@gmail.com>
2026-05-04 20:14:04 -0300

    Iniciando o desenvolvimento da opção "% de requisições por SO"

--- src/main/java/log/analisador/Main.java
+++ src/main/java/log/analisador/Main.java
@@ -1,6 +1,7 @@
 package log.analisador;
 
 import log.analisador.arquivo.ManipulacaoArquivo;
+import log.analisador.interacao.Menu;
 import log.analisador.log.RegexLog;
 
 public class Main {
@@ -11,7 +12,5 @@ public class Main {
         String resultado = teste.buscaRecursoGrande("54.36.148.92 - - [19/Dec/2020:14:16:44 +0100] \"GET /index.php?option=com_phocagallery&view=category&id=2%3Awinterfotos&Itemid=53 HTTP/1.1\" 200 30662 \"-\" \"Mozilla/5.0 (compatible; AhrefsBot/7.0; +http://ahrefs.com/robot/)\" \"-\"\r\n" + //
                         "");
         System.out.println(resultado);
-
-
     }
 }
--- src/main/java/log/analisador/log/RegexLog.java
+++ src/main/java/log/analisador/log/RegexLog.java
@@ -6,14 +6,14 @@ import java.util.regex.Pattern;
 public class RegexLog {
     
     public String buscaRecursoGrande(String linha) {
-        Pattern pattern = Pattern.compile("([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}).*?\".*?\" ([0-9]{3}) ([0-9]+)");
+        Pattern pattern = Pattern.compile("([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}) \\[.*?\\] \"[^\"]*\" ([0-9]{3}) ([0-9]+)");
         Matcher matcher = pattern.matcher(linha);
-
 
         if (matcher.find()) {
             String ip = matcher.group(1);
             int status = Integer.parseInt(matcher.group(2));
             long tamanho = Long.parseLong(matcher.group(3));
+
             if (status >= 200 && status <= 299 && tamanho > 2000) {
                 return status + " " + tamanho + " " + ip;
             }
@@ -21,15 +21,9 @@ public class RegexLog {
         return null;
     }
     
-    public String naoRespondidosNov(String linha) {    	
-    	
-    	//linha = "157.48.153.185 - - [19/Dec/2020:14:08:08 +0100] \"GET /favicon.ico HTTP/1.1\" 404 217 \"http://www.almhuette-raith.at/apache-log/access.log\" \"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36\" \"-\"\r\n";
-    	
-    	Pattern pattern = Pattern.compile(".*?\\- \\- \\[[0-9]{2}/(Dec/2020).*?\".*?\" ([0-9]{3}) [0-9]+ (\"http[^\"]+\")");
-
-    	
-    	//Pattern pattern = Pattern.compile(".*?\\- \\- \\[[0-9]{2}/(Dec/2020):[0-9]{2}:[0-9]{2}:[0-9]{2} \\+[0-9]{4}\\] \".?\" ([0-9]{3}) .?\n(\"http(.+)\")");
-    	Matcher matcher = pattern.matcher(linha);
+    public String naoRespondidosNov(String linha) {    	    	
+    	Pattern pattern = Pattern.compile(".*?\\- \\- \\[[0-9]{2}/(Nov/2021).*?\".*?\" ([0-9]{3}) [0-9]+ (\"http[^\"]+\")");
+      	Matcher matcher = pattern.matcher(linha);
     	
     	if (matcher.find()) {
     		String data = matcher.group(1);
