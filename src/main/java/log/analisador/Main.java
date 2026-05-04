538af83fa19d022e47653a1e209bd52aa013b254
EricFelix <ericfelixfelixpinto@gmail.com>
2026-05-04 20:09:44 -0300

    Resolvendo o direcionamento dos arquivos

--- src/main/java/log/analisador/Main.java
+++ src/main/java/log/analisador/Main.java
@@ -1,16 +1,26 @@
 package log.analisador;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import log.analisador.arquivo.ManipulacaoArquivo;
-import log.analisador.log.RegexLog;
 
 public class Main {
     public static void main(String[] args) {
-        //ManipulacaoArquivo teste = new ManipulacaoArquivo();
-        //teste.manipulaLog();
-        RegexLog teste = new RegexLog();
-        String resultado = teste.buscaRecursoGrande("54.36.148.92 - - [19/Dec/2020:14:16:44 +0100] \"GET /index.php?option=com_phocagallery&view=category&id=2%3Awinterfotos&Itemid=53 HTTP/1.1\" 200 30662 \"-\" \"Mozilla/5.0 (compatible; AhrefsBot/7.0; +http://ahrefs.com/robot/)\" \"-\"\r\n" + //
-                        "");
-        System.out.println(resultado);
+        Path path = Paths.get("Analise");
+
+        try {
+            Files.createDirectories(path);
+            System.out.println("Pasta 'Analise' criada com sucesso!");
+        } catch (IOException e) {
+            System.err.println("Erro ao criar a pasta: " + e.getMessage());
+        }
+
+
+        ManipulacaoArquivo teste = new ManipulacaoArquivo();
+        teste.manipulaLog();
 
 
     }
--- src/main/java/log/analisador/arquivo/ManipulacaoArquivo.java
+++ src/main/java/log/analisador/arquivo/ManipulacaoArquivo.java
@@ -1,30 +1,18 @@
 package log.analisador.arquivo;
-import log.analisador.log.*;
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import log.analisador.log.RegexLog;
 
 public class ManipulacaoArquivo{
     
     public void manipulaLog(){
         RegexLog regex = new RegexLog();
-        Path path = Paths.get("Analise");
-
-        try {
-            Files.createDirectories(path);
-            System.out.println("Pasta 'Analise' criada com sucesso!");
-        } catch (IOException e) {
-            System.err.println("Erro ao criar a pasta: " + e.getMessage());
-        }
         try {
             BufferedReader arquivo = new BufferedReader(new FileReader("access.log"));
-            BufferedWriter escritor = new BufferedWriter(new FileWriter("recursosGrandes.txt"));
+            BufferedWriter escritor = new BufferedWriter(new FileWriter("Analise/recursosGrandes.txt"));
             String linha;
             String resultado;
             while((linha = arquivo.readLine())!= null){
