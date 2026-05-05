8deb17c9476b5a7e8eb577a8343ab7b6b7bde427
Fabi <fabistudy016@gmail.com>
2026-05-04 22:21:01 -0300

    Continuação da opção 3 "% das requisições por SO"

--- src/main/java/log/analisador/log/RegexLog.java
+++ src/main/java/log/analisador/log/RegexLog.java
@@ -36,5 +36,29 @@ public class RegexLog {
     	}
     	return null;
     }
+    
+    public String porcRequisicaoSO(String linha) {
+    	//linha = "42.236.10.125 - - [19/Dec/2020:15:23:10 +0100] \"GET / HTTP/1.1\" 200 10479 \"http://baidu.com/\" \"Mozilla/5.0 (Linux; U; Android 8.1.0; zh-CN; EML-AL00 Build/HUAWEIEML-AL00) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.108 baidu.sogo.uc.UCBrowser/11.9.4.974 UWS/2.13.1.48 Mobile Safari/537.36 AliApp(DingTalk/4.5.11) com.alibaba.android.rimet/10487439 Channel/227200 language/zh-CN\" \"-\"\r\n";
+    	
+    	Pattern pattern = Pattern.compile(".*?\\- \\- \\[[0-9]{2}/[A-Z]{1}[a-z]{2}/(2020).*?\".*?\" .*?\".*?\" .*?\\(([A-Z|iP]{1,2}[a-z]{1,20}).*?");
+    	//Pattern pattern = Pattern.compile(".*?\\- \\- \\[[0-9]{2}/[A-Z]{1}[a-z]{2}/2020.*?\".*?\" .*?\".*?\" \\(([A-Z]{1}[a-z]{1,10});((.*?)Android) ((.*?)Mobile)");
+      	Matcher matcher = pattern.matcher(linha);
+    	
+    	if (matcher.find()) {
+    		String d = matcher.group(1);
+    		String sistOp = matcher.group(2);
+    		String mobile = "Mobile";
+    		
+    		return d + " " + sistOp;
+    		
+    		/*
+    		if (android == "Android" || mobile == "Mobile") {
+    			return sistOp;
+    		}
+    		*/
+    	}
+    	
+    	return null;
+    }
 
 }
